7ecd60c8c41a123a12512a54b0d739729dd5fc95
RoieHub <roiemalykin@gmail.com>
2020-08-20 16:56:19 +0300

    node_data interface added

---
+++ cmake-build-debug/CMakeFiles/TahoBus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TahoBus.dir/main.cpp.obj"
+  "TahoBus.exe"
+  "TahoBus.exe.manifest"
+  "TahoBus.pdb"
+  "libTahoBus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TahoBus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TahoBus.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TahoBus.
+# This may be replaced when dependencies are built.
