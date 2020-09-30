42285f371257359dd2a2cd689d8a548c0833722e
RoieHub <roiemalykin@gmail.com>
2020-09-30 13:36:48 +0300

    Point_3D.h with basic operations , unimplemented.

--- Point_3D.h
+++ Point_3D.h
@@ -4,17 +4,28 @@
 
 #ifndef TAHOBUS_POINT_3D_H
 #define TAHOBUS_POINT_3D_H
-Class Point_3D
+#include<iostream>
+class Point_3D
 {
+private:
 // Member
-std::tuple<double,double,double> _location;
+std::tuple<double,double,double> _location   ; // Need to find what the bug
+
+    bool set_first(double x);
+
+    bool set_second(double x);
+
+    bool set_third(double x) ;
+public:
+// Constructor
+Point_3D(double x,double y,double z)
+    {
+    set_first(x);
+    set_second(y);
+    set_third(z);
+    }
 
 // Operations
-bool set_first(double x);
-
-bool set_second(double x);
-
-bool set_third(double x) ;
 
 const double get_first();
 const double get_second();
